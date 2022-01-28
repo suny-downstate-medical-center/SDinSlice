023cdb99bda72400a2e1761fed382ee65f202122
craig <kelley92@bu.edu>
2022-01-28 14:47:35 -0500

    netParams.py: # v0.09 - replicates results from SpatialModel.py

---
+++ SpatialModelNetpyne/cfg.py
@@ -0,0 +1,73 @@
+from netpyne import specs
+import numpy as np
+#------------------------------------------------------------------------------
+#
+# SIMULATION CONFIGURATION
+#
+#------------------------------------------------------------------------------
+
+# Run parameters
+cfg = specs.SimConfig()       # object of class cfg to store simulation configuration
+cfg.duration = 2e3        # Duration of the simulation, in ms
+cfg.hParams['v_init'] = -70.0   # set v_init to -65 mV
+cfg.hParams['celsius'] = 37.0
+cfg.dt = 0.1              # Internal integration timestep to use
+cfg.verbose = False            # Show detailed messages 
+cfg.recordStep = 1             # Step size in ms to save data (eg. V traces, LFP, etc)
+cfg.filename = 'sim_setup/'   # Set file output name
+cfg.Kceil = 15.0
+
+ # Network dimensions
+cfg.sizeX = 500 #250.0 #1000
+cfg.sizeY = 500 #250.0 #1000
+cfg.sizeZ = 200.0
+cfg.density = 90000.0
+cfg.Vtissue = cfg.sizeX * cfg.sizeY * cfg.sizeZ
+
+# slice conditions 
+cfg.ox = 'hypoxic' #'perfused'
+if cfg.ox == 'perfused':
+    cfg.o2_bath = 0.1
+    cfg.alpha_ecs = 0.2 
+    cfg.tort_ecs = 1.6
+elif cfg.ox == 'hypoxic':
+    cfg.o2_bath = 0.01
+    cfg.alpha_ecs = 0.07 
+    cfg.tort_ecs = 1.8
+
+cfg.prep = 'invitro' #'invivo'
+
+# neuron params 
+cfg.betaNrn = 0.24
+cfg.Ncell = int(cfg.density*(cfg.sizeX*cfg.sizeY*cfg.sizeZ*1e-9)) # default 90k / mm^3
+if cfg.density == 90000.0:
+    cfg.rs = ((cfg.betaNrn * cfg.Vtissue) / (2 * np.pi * cfg.Ncell)) ** (1/3)
+else:
+    cfg.rs = 7.52
+
+cfg.epas = -70 # False
+cfg.gpas = 0.0001
+cfg.sa2v = 3.0 # False
+if cfg.sa2v:
+    cfg.somaR = (cfg.sa2v * cfg.rs**3 / 2.0) ** (1/2)
+else:
+    cfg.somaR = cfg.rs
+cfg.cyt_fraction = cfg.rs**3 / cfg.somaR**3
+
+# Recording/plotting parameters
+cfg.recordCells = [('E', [0,50,100,150,200,250,300,350,400,450])]
+cfg.recordTraces = {'V_soma':{'sec': 'soma','loc': 0.5,'var': 'v'}}#,
+                        #   'ik_soma': {'sec': 'soma', 'loc': 0.5, 'var': 'ik'},
+                        #   'cai_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cai'},
+                        #   'cao_soma': {'sec': 'soma', 'loc':0.5, 'var': 'cao'}}
+cfg.analysis['plotTraces'] = {'include': [('E', i*25) for i in range(20)], 'oneFigPer': 'cell', 'figSize': (10,4), 'saveFig': True, 'showFig': False}
+
+# cfg.recordLFP = [[-15, y, 1.0*cfg.sizeZ] for y in range(int(cfg.sizeY/3), int(cfg.sizeY), int(cfg.sizeY/3))]
+
+# cfg.anacfg.sizeYsis['plotTraces']={'include': random.sample(range(cfg.Ncell),100), 'saveFig' : True}
+# cfg.anacfg.sizeYsis['plotTraces']={'include': random.sample(range(cfg.Ncell),20), 'saveFig' : True, 'showFig' : False}
+# cfg.anacfg.sizeYsis['plotRaster'] = {'orderBy': 'y', 'orderInverse': True, 'saveFig': True, 'figSize': (9,3), 'showFig' : False}      # Plot a raster
+# cfg.anacfg.sizeYsis['plot2Dnet'] = {'saveFig': True, 'showFig' : False}   
+#cfg.anacfg.sizeYsis['plotLFP'] = {'includeAxon': False, 'figSize': (6,10), 'NFFT': 256, 'noverlap': 48, 'nperseg': 64, 'saveFig': True} 
+# cfg.anacfg.sizeYsis['plotRxDConcentration'] = {'speciesLabel': 'k', 'regionLabel': 'ecs', 'showFig' : False}
+ 
