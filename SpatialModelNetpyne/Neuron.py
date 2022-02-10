8d5b31f4faa281bab57326377e35a0cd00c90fcf
craig <kelley92@bu.edu>
2022-02-10 10:20:19 -0500

    generic Emodel in Neuron.py

--- SpatialModelNetpyne/Neuron.py
+++ SpatialModelNetpyne/Neuron.py
@@ -2,6 +2,20 @@ from neuron import h
 from cfg import cfg 
 import numpy as np 
 
+class ENeuron:
+    """ A neuron with soma and dendrite with; fast and persistent sodium
+    currents, potassium currents, passive leak and potassium leak and an
+    accumulation mechanism. """
+    def __init__(self):
+        self.soma = h.Section(name='Esoma', cell=self)
+        # add 3D points to locate the neuron in the ECS  
+        self.soma.pt3dadd(0.0, 0.0, 0.0, 2.0 * cfg.somaR)
+        self.soma.pt3dadd(0.0, 2.0 * cfg.somaR, 0.0, 2.0 * cfg.somaR)
+        if cfg.epas:
+            self.soma.insert('pas')
+            self.soma(0.5).pas.e = cfg.epas
+            self.soma(0.5).pas.g = cfg.gpas
+
 class E2Neuron:
     """ A neuron with soma and dendrite with; fast and persistent sodium
     currents, potassium currents, passive leak and potassium leak and an
