fdd193f3b103d516924415ac160cc6491521aec8
craig <kelley92@bu.edu>
2022-05-04 16:22:28 -0400

    fix fig origin

--- analysis.py
+++ analysis.py
@@ -443,16 +443,17 @@ def centerVsPeriphKspeed(datadir, dur, rmax=600):
         periph_speed = m / 16.667
     return core_speed, periph_speed
 
-def billKgraph(datadir, dur=10, depth=8):
+def billKgraph(datadir, dur=10, depth=8, title='Center'):
     k_files = ['k' +'_'+str(i)+'.npy' for i in range(int(dur*1000)) if (i%100)==0] 
     kconcs = []
     for k_file in k_files:
         data = np.load(datadir+k_file)
         kconcs.append(list(data[:,:,depth][20][21:]))
     time = list(np.linspace(0,dur,len(kconcs)))
-    plt.imshow(kconcs, extent=(0,dur,0, 500), aspect='auto')
+    plt.imshow(kconcs, extent=(0,dur,0, 500), aspect='auto', origin='lower')
     plt.xlabel('Time (s)')
     plt.ylabel('Radial Distance (um)')
+    plt.title(title)
 
 def allSpeciesMov(datadir, outpath, vmins, vmaxes, figname, condition='Perfused', dur=10, extent=None, fps=40):
     """"Generates an mp4 video with heatmaps for K+, Cl-, Na+, and O2 overlaid with spiking data"""
